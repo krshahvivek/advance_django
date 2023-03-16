5ca8e30d20e8d3bf56c619c4045e2b97a9429e42
vivek kumar <krshahvivek@gmail.com>
2023-03-17 00:05:39 +0530

    urls file changes

--- GoDigApp/urls.py
+++ GoDigApp/urls.py
@@ -9,9 +9,7 @@ app_name = "GoDigApp"
 urlpatterns=[
   path('savePetroliumPrice/', views.saveCurrentPetroliumPrices),
   path('enrollUser/',views.enrollUser),
-  # path('getPasscode/',views.getPasscode),
   path('verifyAllFields/',views.verifyAllFields,name='verifyAllFields'),
-  # path('verifyPasscode/',views.verify_passcode,name='verifyPasscode'),
   path('registration/',views.registration,name='registration'),
   path('login/',views.login,name='login'),
   path('csvToTable/',views.CSVIntoProductionTable,name='CSVIntoProductionTable'),
